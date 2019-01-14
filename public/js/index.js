9bd82bdb0adb002c245be8cb12da885182d00200
Joonho Ko <joonhok@mit.edu>
2019-01-13 20:57:51 -0500

    socketified what we already have

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,27 @@
+var socket = io();
+
 window.onload = () => {
   document.onkeypress = (e) => {
     switch (e.keyCode) {
       case 113:
-        playSound('h1');
+        socket.emit('play sound', 'h1');
         break;
       case 119:
-        playSound('h2');
+        socket.emit('play sound', 'h2');
         break;
       case 101:
-        playSound('h3');
+        socket.emit('play sound', 'h3');
         break;
       case 114:
-        playSound('h4');
+        socket.emit('play sound', 'h4');
         break;
       default:
         console.log(e.keyCode);
     }
   }
 }
+
+socket.on('play sound', sound => {
+  console.log('recieved sound ' + sound);
+  playSound(sound);
+})
--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const views = require('./routes/views');
 
 app.use('/', views);
 app.use('/static', express.static('public'));
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+io.on('connection', function(socket){
+  socket.on('play sound', sound => {
+    io.emit('play sound', sound);
+  })
+});
+
+http.listen(3000, () => console.log('App listening on port 3000!'));
