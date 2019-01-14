498aa47bcfc2675a8aa19943258c550123f08a05
MFarejowicz <mfarejowicz@gmail.com>
2019-01-14 13:50:34 -0500

    add basic visuals

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,35 @@
-var socket = io();
+const socket = io();
+
+function removeFadeOut(el, speed) {
+  const seconds = speed / 1000;
+  el.style.transition = `opacity ${seconds}s ease`;
+  setTimeout(() => el.style.opacity = 0, 0);
+  setTimeout(() => el.parentNode.removeChild(el), speed);
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function spawnStar() {
+  let top = document.getElementById('top');
+  let width = top.offsetWidth-50;
+  let height = top.offsetHeight-50;
+
+  let star = document.createElement('img');
+  let x = getRandomArbitrary(0, width);
+  let y = getRandomArbitrary(0, height);
+  star.setAttribute('src', '/static/img/star.png');
+  star.setAttribute('class', 'star');
+  star.style.top = `${y}px`;
+  star.style.left = `${x}px`;
+
+  top.appendChild(star);
+  removeFadeOut(star, 2000);
+}
 
 window.onload = () => {
+  let prompt = true;
   document.onkeypress = (e) => {
     switch (e.keyCode) {
       case 113:
@@ -18,10 +47,17 @@ window.onload = () => {
       default:
         console.log(e.keyCode);
     }
+
+    if (prompt) {
+      removeFadeOut(document.getElementById('prompt'), 1500);
+      prompt = false;
+    }
+
+    spawnStar();
   }
 }
 
 socket.on('play sound', sound => {
-  console.log('recieved sound ' + sound);
+  console.log('received sound ' + sound);
   playSound(sound);
 })
