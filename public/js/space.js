c3f0a249fd6f75cb87c28fda49416af3dbedaa84
MFarejowicz <mfarejowicz@gmail.com>
2019-01-19 00:09:09 -0500

    sync visuals, add color

--- public/js/space.js
+++ public/js/space.js
@@ -1,12 +1,5 @@
 const socket = io();
 
-function fadeOutAndRemove(el, speed) {
-  const seconds = speed / 1000;
-  el.style.transition = `opacity ${seconds}s ease`;
-  setTimeout(() => el.style.opacity = 0, 100);
-  setTimeout(() => el.parentNode.removeChild(el), speed);
-}
-
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -17,138 +10,126 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function spawnStar() {
-  let top = document.getElementById('top');
-  let width = top.offsetWidth-50;
-  let height = top.offsetHeight-50;
-
-  let star = document.createElement('img');
-  let x = getRandom(0, width);
-  let y = getRandom(0, height);
-  let num = getRandomInt(1, 3)
-  star.setAttribute('src', `/static/img/star${num}.png`);
-  star.setAttribute('class', 'star');
-  star.style.top = `${y}px`;
-  star.style.left = `${x}px`;
-
-  top.appendChild(star);
-  fadeOutAndRemove(star, 2000);
-}
-
-function spawnPlanet() {
-  let top = document.getElementById('top');
-  let width = top.offsetWidth-50;
-  let height = top.offsetHeight-50;
-
-  let star = document.createElement('img');
-  let x = getRandom(0, width);
-  let y = getRandom(0, height);
-  let num = getRandomInt(1, 5)
-  star.setAttribute('src', `/static/img/planet${num}.png`);
-  star.setAttribute('class', 'star');
-  star.style.top = `${y}px`;
-  star.style.left = `${x}px`;
-
-  top.appendChild(star);
-  fadeOutAndRemove(star, 10000);
-}
-
-function spawnSun() {
-  let top = document.getElementById('top');
-  let width = top.offsetWidth-100;
-  let height = top.offsetHeight-100;
-
-  let star = document.createElement('img');
-  let x = getRandom(0, width);
-  let y = getRandom(0, height);
-  star.setAttribute('src', `/static/img/sun.png`);
-  star.setAttribute('class', 'star');
-  star.style.top = `${y}px`;
-  star.style.left = `${x}px`;
-
-  top.appendChild(star);
-  fadeOutAndRemove(star, 20000);
-}
-
-function spawnRandom() {
-  let rand = Math.random()
-  if (rand < 0.98) {
-    spawnStar()
-  } else if (rand < 0.995) {
-    spawnPlanet()
-  } else {
-    spawnSun()
-  }
-}
-
 function joinPopup() {
   const joinPopup = document.getElementById('join-popup');
   joinPopup.classList.toggle('show');
   joinPopup.classList.toggle('hide');
 }
 
+function fadeOutAndRemove(el, speed) {
+  const seconds = speed / 1000;
+  el.style.transition = `opacity ${seconds}s ease`;
+  setTimeout(() => el.style.opacity = 0, 100);
+  setTimeout(() => el.parentNode.removeChild(el), speed);
+}
+
+function appendSpawn(spawnInfo, hue) {
+  let top = document.getElementById('top');
+  let spawn = document.createElement('img');
+
+  spawn.setAttribute('src', `/static/img/${spawnInfo.type}${spawnInfo.num}.png`);
+  spawn.setAttribute('class', 'spawn');
+  spawn.style.top = `${spawnInfo.y}%`;
+  spawn.style.left = `${spawnInfo.x}%`;
+  spawn.style.filter = `hue-rotate(${hue}deg) saturate(3)`;
+
+  top.appendChild(spawn);
+  fadeOutAndRemove(spawn, spawnInfo.time);
+}
+
+function chooseSpawn() {
+  let rand = Math.random();
+  let type, time, num, x, y;
+
+  if (rand < 0.98) {
+    type = 'star';
+    time = 4000;
+    num = getRandomInt(1, 3);
+    x = getRandom(0, 90);
+    y = getRandom(0, 90);
+  } else if (rand < 0.995) {
+    type = 'planet';
+    time = 10000;
+    num = getRandomInt(1, 5);
+    x = getRandom(0, 80);
+    y = getRandom(0, 80);
+  } else {
+    type = 'sun';
+    time = 20000;
+    num = 1;
+    x = getRandom(0, 70);
+    y = getRandom(0, 70);
+  }
+
+  return { type, time, num, x, y }
+}
+
 window.onload = () => {
   let prompt = true;
+  let hue = getRandomInt(0, 360);
+
   document.onkeypress = (e) => {
     const joinInput = document.getElementById('join-input');
     if (!(joinInput === document.activeElement)) {
+      const spawnInfo = chooseSpawn();
+
       switch (e.keyCode) {
         case 113:
-          socket.emit('play sound', 'h1');
+          socket.emit('play sound', 'h1', spawnInfo, hue);
           break;
         case 119:
-          socket.emit('play sound', 'h2');
+          socket.emit('play sound', 'h2', spawnInfo, hue);
           break;
         case 101:
-          socket.emit('play sound', 'h3');
+          socket.emit('play sound', 'h3', spawnInfo, hue);
           break;
         case 114:
-          socket.emit('play sound', 'h4');
+          socket.emit('play sound', 'h4', spawnInfo, hue);
           break;
         case 122:
-          socket.emit('play sound', 'C4');
+          socket.emit('play sound', 'C4', spawnInfo, hue);
           break;
         case 120:
-          socket.emit('play sound', 'E4');
+          socket.emit('play sound', 'E4', spawnInfo, hue);
           break;
         case 99:
-          socket.emit('play sound', 'G4');
+          socket.emit('play sound', 'G4', spawnInfo, hue);
           break;
         case 118:
-          socket.emit('play sound', 'B4');
+          socket.emit('play sound', 'B4', spawnInfo, hue);
           break;
         case 108:
-          socket.emit('play sound', 'neosweep');
+          socket.emit('play sound', 'neosweep', spawnInfo, hue);
           break;
         case 97:
-          socket.emit('play sound', 'pew');
+          socket.emit('play sound', 'pew', spawnInfo, hue);
           break;
         case 112:
-          socket.emit('play sound', 'bass');
+          socket.emit('play sound', 'bass', spawnInfo, hue);
           break;
         case 111:
-          socket.emit('play sound', 'chipkick');
+          socket.emit('play sound', 'chipkick', spawnInfo, hue);
           break;
         case 109:
-          socket.emit('play sound', 'piano5');
+          socket.emit('play sound', 'piano5', spawnInfo, hue);
           break;
         case 110:
-          socket.emit('play sound', 'xylochord');
+          socket.emit('play sound', 'xylochord', spawnInfo, hue);
           break;
         case 115:
-          socket.emit('play sound', 'jazzy1');
+          socket.emit('play sound', 'jazzy1', spawnInfo, hue);
           break;
         case 107:
-          socket.emit('play sound', 'warm1');
+          socket.emit('play sound', 'warm1', spawnInfo, hue);
           break;
         case 102:
-          socket.emit('play sound', 'pluckFs5');
+          socket.emit('play sound', 'pluckFs5', spawnInfo, hue);
           break;
         case 103:
-          socket.emit('play sound', 'pluckGs5');
+          socket.emit('play sound', 'pluckGs5', spawnInfo, hue);
           break;
         case 104:
-          socket.emit('play sound', 'pluckAs5');
+          socket.emit('play sound', 'pluckAs5', spawnInfo, hue);
           break;
         default:
           console.log(e.keyCode);
@@ -177,14 +158,17 @@ window.onload = () => {
 
 socket.on('connect', () => {
   const spacecode = window.location.pathname.slice(7);
-  console.log(spacecode);
+
   if (spacecode.length === 4) {
+    console.log(`Joining ${spacecode}`);
     socket.emit('join room', spacecode);
   } else if (spacecode.length === 0) {
+    console.log(`Joining default`);
     socket.emit('join room', 'default');
   }
+
   if (document.getElementById('logout')) {
-    // Make post request to track taps
+    // Make post request to track joins
     axios.post('/api/join')
     .catch((error) => {
       console.log(error);
@@ -192,8 +176,8 @@ socket.on('connect', () => {
   }
 });
 
-socket.on('play sound', (sound) => {
-  console.log('received sound ' + sound);
+socket.on('play sound', (sound, spawn, hue) => {
+  console.log(`received sound ${sound}`);
   playSound(sound);
-  spawnRandom();
+  appendSpawn(spawn, hue);
 });
