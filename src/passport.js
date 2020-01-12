159be490c1982ea8fc3794fe29059e222c3c7d6c
Matthew Farejowicz <mfarejow@mit.edu>
2020-01-12 18:10:19 -0500

    try even diff url

--- src/passport.js
+++ src/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      callbackURL: "http://sound-space.herokuapp.com/auth/github/callback"
+      callbackURL: "https://sound-space.herokuapp.com/auth/github/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log(`User ${profile.username} logged in.`);
